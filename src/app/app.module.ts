8f77ecde38df6aaeb31095ade50c1757e59ca0b5
Tanner Hart <tannerhart24@yahoo.com>
2022-06-08 11:29:57 -0700

    Set up routing within the app and well as pulled our first books.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HttpClient} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './components/books/books.component';
+import { HeaderComponent } from './constants/header/header.component';
+import { FooterComponent } from './constants/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
